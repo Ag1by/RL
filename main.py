b91e3357033e26a0315c63a2e1b733057268d489
Ag <smicbjrichard@163.com>
2023-09-14 11:03:32 +0800

    create main

---
+++ main.py
@@ -0,0 +1,21 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
+import uvicorn
+
+async def homepage(request):
+    return JSONResponse({"hello":"world"})
+async def about(request):
+    return JSONResponse({"message":"umamusumei"})
+async def contect(request):
+    return JSONResponse({"my number":"110"})
+app = Starlette(
+    debug = True,
+    routes = [
+        Route('/',homepage),
+        Route('/message',about),
+        Route('contect',contects)
+    ]
+)
+uvicorn.run(app,port=8080)
